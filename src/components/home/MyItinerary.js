86d9215409e099efba419de5e35b3978d05f4ce1
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-10-01 14:15:14 -0500

    delete parks

--- src/components/home/MyItinerary.js
+++ src/components/home/MyItinerary.js
@@ -5,6 +5,13 @@ import React, { useEffect, useState } from "react"
 // presses yes, send a DELETE
 // request to the API to remove that itinerary item.
 
+// When the itinerary for the current customer is listed, put an edit button next
+// to each link. When that button is clicked, present a dialog that lets the user
+// enter in a new time for that itinerary item and has an Update button in it. When
+// the update button is clicked, send a PUT request to the API to update the starttime
+//  attribute of that itinerary item.
+
+
 const MyItinerary = props => {
     const [itineraryList, setItineraryList] = useState([])
 
@@ -61,6 +68,7 @@ const deleteFromItinerary = (id) => {
                     itineraryList.map((item) => {
                         return <li key={item.id}>
                             {item.attraction.name} at {item.starttime}
+                            <button>edit</button>
                             <button
                              onClick={() => deleteFromItinerary(item.id)}
                             >delete</button>
